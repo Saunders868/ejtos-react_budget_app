79c203329ec0dc9295f2743e9f444aae91769c41
Daniel Saunders <danielsaunders@Daniels-MacBook-Pro.local>
2024-02-17 22:02:29 -0400

    final commit

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,18 +1,33 @@
-
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import {
+  BUDGET_TOO_HIGH_ERROR_MESSAGE,
+  BUDGET_TOO_LOW_ERROR_MESSAGE,
+} from "../constants";
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+  const { budget, currency } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
+  const handleBudgetChange = (event) => {
+    if (event.target.value > 20000) {
+      alert(BUDGET_TOO_HIGH_ERROR_MESSAGE);
+    } else if (event.target.value < 960) {
+      alert(BUDGET_TOO_LOW_ERROR_MESSAGE);
+    } else {
+      setNewBudget(event.target.value);
     }
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-        </div>
-    );
+  };
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: {currency}</span>
+      <input
+        type="number"
+        max={20000}
+        step="10"
+        value={newBudget}
+        onChange={handleBudgetChange}
+      ></input>
+    </div>
+  );
 };
 export default Budget;
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const BUDGET_TOO_HIGH_ERROR_MESSAGE = "Your budget cannot exceed 20,000";
+export const BUDGET_TOO_LOW_ERROR_MESSAGE =
+  "Your budget cannot be less than the spending";
